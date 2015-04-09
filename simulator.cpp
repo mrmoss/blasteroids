d8161717b42a85f70ff6520e08334c95d12ca813
Your Name <you@example.com>
2015-04-09 09:43:05 -0800

    Simulator adds a simple "asteroid" array...

--- simulator.cpp
+++ simulator.cpp
@@ -3,10 +3,27 @@
 #include "msl/socket.hpp"
 #include "msl/time.hpp"
 
+std::string database_address="127.0.0.1:8080";
+std::string json_get_request="/json_get/";
+std::string json_set_request="/json_set/";
+msl::json simulation;
+
 std::string get_request(const std::string& host,const std::string& name);
 
 int main()
 {
+	simulation["asteroids"]=Json::arrayValue;
+
+	std::cout<<"Requesting \""<<json_get_request<<"\" from database at "<<database_address<<"."<<std::endl;
+	std::cout<<"Received:  "<<get_request(database_address,json_get_request)<<std::endl;
+
+	auto setting=json_set_request+msl::serialize(simulation);
+	std::cout<<"Requesting \""<<setting<<"\" from database at "<<database_address<<"."<<std::endl;
+	std::cout<<"Received:  "<<get_request(database_address,setting)<<std::endl;
+
+	std::cout<<"Requesting \""<<json_get_request<<"\" from database at "<<database_address<<"."<<std::endl;
+	std::cout<<"Received:  "<<get_request(database_address,json_get_request)<<std::endl;
+
 	return 0;
 }
 
