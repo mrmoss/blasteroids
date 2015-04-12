e23e4a32364d31f1cebcdd6a7564c294b19e8111
Your Name <you@example.com>
2015-04-12 13:56:21 -0800

    Need to make database accept post...

--- simulator.cpp
+++ simulator.cpp
@@ -75,7 +75,7 @@ void simulation_initialize()
 {
 	simulation["asteroids"]=Json::arrayValue;
 
-	for(int ii=0;ii<80;++ii)
+	for(int ii=0;ii<50;++ii)
 	{
 		simulation["asteroids"][ii]["sprite"]=asteroid_sprites[random_int(0,2)];
 		simulation["asteroids"][ii]["pos"]["x"]=random_double(100,200);
